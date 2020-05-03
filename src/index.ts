f58bdf33d36212cd5ae8f58588168ee151e4ab71
Muhammad Talha <talhaguy@gmail.com>
2020-05-03 12:44:24 -0700

    setting up initial typescript build

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+import { greet } from './greet';
+
+const app = express();
+const port = process.env.PORT || '8000';
+
+app.get('/', (_req: Request, res: Response) => {
+    return res.send(`Home page 🤓 - ${greet("Talha")}`);
+});
+
+app.listen(port, err => {
+    if (err) return console.error(err);
+    return console.log(`Server is listening on ${port}`);
+});
