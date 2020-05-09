4f63cc4b2ba3e72f5fb87b1b0edee0283943f7ec
Muhammad Talha <talhaguy@gmail.com>
2020-05-09 14:59:09 -0700

    removing explicit database factories as i am now keeping them in the index files

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,24 +1,25 @@
 import {
-    connectFactory,
-    connectionErrorCbFactory,
-    connectionOpenCbFactory,
-} from "./db.factory"
-import {
     connect as _connect,
     connectionErrorCb as _connectionErrorCb,
     connectionOpenCb as _connectionOpenCb,
 } from "./db"
-import mongoose from "mongoose"
+import mongoose, { Mongoose } from "mongoose"
 
-const connectionErrorCb = connectionErrorCbFactory(_connectionErrorCb, console)
-const connectionOpenCb = connectionOpenCbFactory(_connectionOpenCb, console)
+const connectionErrorCb = ((console: Console) => (err: any) =>
+    _connectionErrorCb(console, err))(console)
 
-export const connect = connectFactory(
-    _connect,
-    mongoose,
-    process.env["DB_PATH"],
-    connectionErrorCb,
-    connectionOpenCb
-)
+const connectionOpenCb = ((console: Console) => () =>
+    _connectionOpenCb(console))(console)
+
+export const connect = ((
+    mongoose: Mongoose,
+    dbPath: string,
+    connectionErrorCb: (err: any) => void,
+    connectionOpenCb: () => void
+) => {
+    return () => {
+        return _connect(mongoose, dbPath, connectionErrorCb, connectionOpenCb)
+    }
+})(mongoose, process.env["DB_PATH"], connectionErrorCb, connectionOpenCb)
 
 export { database } from "./db"
