eae36f5b46dedbb894af851cbd663941c696bb78
Muhammad Talha <talhaguy@gmail.com>
2020-05-15 22:16:20 -0700

    fixing App test

--- client-src/App.spec.tsx
+++ client-src/App.spec.tsx
@@ -1,11 +1,27 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import { App } from "./App"
+import { DependenciesContext } from "./DependenciesContext"
+import { InitialClientData } from "../shared-src/models"
+import { mock, instance, resetCalls } from "ts-mockito"
 
 describe("App", () => {
+    const initialClientDataMock = mock<InitialClientData>()
+    const initialClientData = instance(initialClientDataMock)
+
+    beforeEach(() => {
+        resetCalls(initialClientDataMock)
+    })
+
     it("should render `App...` text", () => {
-        const component = render(<App />)
-        const text = component.getByText(/App\.\.\./)
+        initialClientData.username = null
+        const component = render(
+            <DependenciesContext.Provider value={initialClientData}>
+                <App />
+            </DependenciesContext.Provider>
+        )
+
+        const text = component.getByText(/Log in/)
         expect(text).toBeInTheDocument()
     })
 })
