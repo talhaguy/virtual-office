420297741c850d9ea77491331b7a4b6b154e8b61
Muhammad Talha <talhaguy@gmail.com>
2020-06-17 19:33:37 -0700

    adding logoutRouteHandlers tests

--- src/routes/indexPageRouteHandler.spec.ts
+++ src/routes/indexPageRouteHandler.spec.ts
@@ -1,45 +1,35 @@
 import { indexPageHandler } from "./indexPageRouteHandler"
-import { mock, instance, resetCalls, verify, deepEqual } from "ts-mockito"
 import { Response, Request } from "express"
 
 describe("indexPageRouteHandler", () => {
     describe("indexPageHandler()", () => {
-        const requestMock = mock<Request>()
-        const request = instance(requestMock)
-        const responseMock = mock<Response>()
-        const response = instance(responseMock)
+        const username = "user@site.com"
+
+        const req = ({
+            user: {
+                username,
+            },
+            flash: jest.fn().mockImplementation(() => ({
+                someKey: ["First message", "Second Message"],
+            })),
+        } as unknown) as Request
+
+        const res = ({
+            render: jest.fn(),
+        } as unknown) as Response
 
         beforeEach(() => {
-            resetCalls(requestMock)
-            resetCalls(responseMock)
+            jest.clearAllMocks()
         })
 
         it("should render the index view file with the initial client data", () => {
-            // send no data if user is not logged in
-            let expectedInitialClientData = {}
-            indexPageHandler(request, response)
-            verify(
-                responseMock.render(
-                    "index",
-                    deepEqual(expectedInitialClientData)
-                )
-            ).once()
-
-            // send username if user is logged in
-            resetCalls(responseMock)
-            request.user = {
-                username: "yuna@spira.com",
-            }
-            expectedInitialClientData = {
-                username: "yuna@spira.com",
-            }
-            indexPageHandler(request, response)
-            verify(
-                responseMock.render(
-                    "index",
-                    deepEqual(expectedInitialClientData)
-                )
-            )
+            indexPageHandler(req, res)
+            expect(res.render).toBeCalledWith("index", {
+                username,
+                flashMessages: {
+                    someKey: ["First message", "Second Message"],
+                },
+            })
         })
     })
 })
